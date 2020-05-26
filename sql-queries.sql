598b205d7fb05955892ee66496afaa85956c6cb7
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-26 16:37:55 +0200

    sql queries ok

---
+++ sql-queries.sql
@@ -0,0 +1,59 @@
+SQL queries
+
+Creare, utilizzando phpMyAdmin, un nuovo database hotel ed importare lo schema allegato sotto.
+Sempre utilizzando phpMyAdmin, come fatto in classe eseguire le seguenti queries:
+
+- Seleziona tutti gli ospiti che sono stati identificati con la carta di identità
+
+	SELECT *
+	FROM `ospiti`
+	WHERE `document_type` = 'CI';
+
+
+- Seleziona tutti gli ospiti che sono nati dopo il 1988
+
+	SELECT *
+	FROM `ospiti`
+	WHERE `date_of_birth` >= '1988-01-01';
+
+
+- Seleziona tutti gli ospiti che hanno più di 20 anni (al momento dell’esecuzione della query)
+
+	SELECT *
+	FROM `ospiti`
+	WHERE (YEAR(CURRENT_DATE()) - YEAR(`date_of_birth`)) >= 20;
+
+
+- Seleziona tutti gli ospiti il cui nome inizia con la D
+
+	SELECT *
+	FROM `ospiti`
+	WHERE `name` LIKE 'd%';
+
+
+- Calcola il totale degli ordini accepted
+
+	SELECT SUM(`price`) AS `accepted payments`
+	FROM `pagamenti`
+	WHERE `status` = 'accepted';
+
+
+- Qual è il prezzo massimo pagato?
+
+	SELECT MAX(`price`) AS `highest price paid`
+	FROM `pagamenti`;
+
+
+- Seleziona gli ospiti riconosciuti con patente e nati nel 1975
+
+	SELECT *
+	FROM `ospiti`
+	WHERE `document_type` = 'Driver License'
+	AND YEAR(`date_of_birth`) = '1975';
+
+
+- Quanti posti letto ha l’hotel in totale?
+
+	SELECT SUM(`beds`) AS `total beds`
+	FROM `stanze`;
+
