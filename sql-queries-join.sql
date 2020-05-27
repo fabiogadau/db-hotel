068bc64957281c75d7c8fb8c05231e7efe74c5e9
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-27 21:19:46 +0200

    two more queries ok

--- sql-queries-join.sql
+++ sql-queries-join.sql
@@ -2,7 +2,7 @@ SQL queries parte due
 
 Sempre utilizzando phpMyAdmin, eseguire le seguenti queries:
 
-Come si chiamano gli ospiti che hanno fatto più di due prenotazioni?
+- Come si chiamano gli ospiti che hanno fatto più di due prenotazioni?
 
       SELECT COUNT(`p`.`ospite_id`) AS `tot ospiti`, `p`.`ospite_id`, `o`.`name`, `o`.`lastname`
       FROM `prenotazioni_has_ospiti` AS `p`
@@ -12,7 +12,7 @@ Come si chiamano gli ospiti che hanno fatto più di due prenotazioni?
       HAVING COUNT(`p`.`ospite_id`) > 2;
 
 
-Stampare tutti gli ospiti per ogni prenotazione
+- Stampare tutti gli ospiti per ogni prenotazione
 
       SELECT `o`.`id` AS `id ospite`, `o`.`name`, `o`.`lastname`, `p`.`id` AS `id prenotazione`, `p`.`ospite_id` AS `id ospite in prenotazione`
       FROM `ospiti` AS `o`
@@ -21,40 +21,51 @@ Stampare tutti gli ospiti per ogni prenotazione
       GROUP BY `p`.`id`;
 
 
-Stampare Nome, Cognome, Prezzo e Pagante per tutte le prenotazioni fatte a Maggio 2018
+- Stampare Nome, Cognome, Prezzo e Pagante per tutte le prenotazioni fatte a Maggio 2018
 
 
 
 
-Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
+- Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
 
 
 
 
-Le stanze sono state tutte prenotate almeno una volta? (Visualizzare le stanze non ancora prenotate)
+- Le stanze sono state tutte prenotate almeno una volta? (Visualizzare le stanze non ancora prenotate)
 
-
-
-
+      SELECT `s`.`id` AS `id stanza non prenotata`, `s`.`room_number` AS `stanza non prenotata`
+      FROM `stanze` AS `s`
+      LEFT JOIN `prenotazioni` AS `p`
+      ON `s`.`id` = `p`.`stanza_id`
+      WHERE `p`.`stanza_id` IS NULL;
 
 
 Bonus: Group by
 
-Conta gli ospiti raggruppandoli per anno di nascita
+- Conta gli ospiti raggruppandoli per anno di nascita
 
       SELECT COUNT(*) AS `totale ospiti`, YEAR(`date_of_birth`)
       FROM `ospiti`
       GROUP BY YEAR(`date_of_birth`);
 
 
-Somma i prezzi dei pagamenti raggruppandoli per status
+- Somma i prezzi dei pagamenti raggruppandoli per status
 
       SELECT `status`, SUM(`price`) AS `total`
       FROM `pagamenti`
       GROUP BY `status`;
 
 
-Quante prenotazioni ha fatto l’ospite che ha fatto più prenotazioni?
+- Quante prenotazioni ha fatto l’ospite che ha fatto più prenotazioni?
 
-
+      SELECT `pr`.`ospite_id`,
+      `o`.`name`, 
+      `o`.`lastname`,
+      COUNT(`pr`.`ospite_id`) AS `prenotazioni_ospite`
+      FROM `prenotazioni_has_ospiti` AS `pr`
+      INNER JOIN `ospiti` AS `o`
+      ON `pr`.`ospite_id` = `o`.`id`
+      GROUP BY `pr`.`ospite_id`
+      ORDER BY `prenotazioni_ospite` DESC
+      LIMIT 2;
 
